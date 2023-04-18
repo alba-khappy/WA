62f9a4b77c6bedf7e382025ecb94b4e0efffa63a
albinabist1993@mail.ru <b.hapilin@gateline.ru>
2023-04-18 22:07:05 +0500

    правки

--- header-scroll.js
+++ header-scroll.js
@@ -1,24 +1,23 @@
 window.addEventListener('scroll', function() {
 
+    let oldScrollY = 0;
     let header = document.querySelector(".header__section");
-    let headerList = document.querySelector(".header__nav-list");
 
-    if(pageYOffset >= 5) {
-        header.classList.add("header__section--background");
-        headerList.classList.add("header__nav-list--text-color");
-    }
+    let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    let dY = scrolled - oldScrollY;
 
-    if(pageYOffset >= 100) {
-        header.classList.add("header__section--hide");
+    if ( dY > 0 ){
+        header.classList.remove('header__section--show');
+        header.classList.add('header__section--hide');
     }
+    else if ( window.pageYOffset == 0 ) {
+        header.classList.remove('header__section--hide');
+        header.classList.remove('header__section--show');
+    } 
+    else {
+        header.classList.add('header__section--show');
+        header.classList.remove('header__section--hide');
+    }  
 
-    if(pageYOffset < 100) {
-        header.classList.remove("header__section--hide");
-    }
-
-    if(pageYOffset < 5) {
-        header.classList.remove("header__section--background");
-        headerList.classList.remove("header__nav-list--text-color");
-    }
-    
-  });
+    oldScrollY = scrolled;
+});
