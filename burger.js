8d7d69c94761f449027f547369ad27f0dcad690d
albinabist1993@mail.ru <b.hapilin@gateline.ru>
2023-05-18 15:03:55 +0300

    правки

--- burger.js
+++ burger.js
@@ -1,8 +1,10 @@
 document.querySelector(".header__button-burger").addEventListener("click", function() {
     document.querySelector(".header__nav-list").classList.add("header__nav-list--show"); 
+    document.querySelector(".header__section").classList.add("header__section--burger-opened"); 
     document.querySelector(".header__nav-list").style.left = '0'; 
   });
 
 document.querySelector(".nav-button-close").addEventListener("click", function() {
-    document.querySelector(".header__nav-list").style.left = '100%'; 
+    document.querySelector(".header__nav-list").style.left = '100%';
+    document.querySelector(".header__section").classList.remove("header__section--burger-opened"); 
   });
